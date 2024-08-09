63d756b3faa8df1632264b7b8f769cf77222b7a2
Margarita Ramirez <margarita26.mr@gmail.com>
2024-08-08 20:35:27 -0400

    negative ading

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function RecursiveCount(num = 0){
     console.log(num)
     RecursiveCount(++num)
 }
+
+// Write a recursive function that counts down from a given number n to 1.
+function CountDown(num = 5){
+    if (num <= 1){
+        return num;
+    }
+    console.log(num);
+    CountDown(--num);
+}
